300211dfbd5916323a3ffc4786ca59db7d62ad38
Ilker Ispir <ilker.ispir@alo-tech.com>
2021-10-27 01:43:52 +0300

    fix: modified json

--- backend/go.mod
+++ backend/go.mod
@@ -17,8 +17,11 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/sys v0.0.0-20211025112917-711f33c9992c // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,23 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"os"
+	"encoding/json"
+	"fmt"
 )
+
+type Teams struct {
+    Teams []Team `json:"teams"`
+}
+
+type Team struct {
+    teamId int `json:"teamId"`
+    abbreviation string `json:"abbreviation"`
+    teamName string `json:"teamName"`
+    simpleName string `json:"simpleName"`
+	location string `json:"location"`
+}
 
 func main() {
 	r := gin.Default()
@@ -18,9 +34,14 @@ func main() {
 	})
 
 	r.POST("/teams", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		jsonFile, _ := os.Open("data/teams.json")
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+
+		var teams Teams
+		json.Unmarshal(byteValue, &teams)
+
+		fmt.Println(teams.Teams[0].teamName)
+		c.JSON(http.StatusOK, teams.Teams)
 	})
 
 	r.POST("/players", func(c *gin.Context) {
